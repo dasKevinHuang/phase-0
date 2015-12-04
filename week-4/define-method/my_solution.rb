ba29e448b4c192d07c371333263df2f4c4e1d097
Kevin Huang <kevin.ziwen.huang@gmail.com>
2015-12-04 12:53:26 -0800

    Add 4.3 excercises

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,9 @@
+
+# Define an Empty Method
+
+# I worked on this challenge by myself.
+
+# Your Solution Below
+def do_something(name, size, number)
+	puts "Here are the details of the object: #{name} #{size} #{number}"
+end
---
+++ week-4/variables-methods.rb
@@ -0,0 +1,20 @@
+# Full Name Greeting Program
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your middle name?"
+middle_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts "Hello " + first_name + " " + middle_name + " " + last_name + ", it's nice to meet you!"
+
+#Bigger, better favorite number
+puts "What is your favorite number?"
+num = gets.to_i
+better_num = num + 1
+puts "Well, I think #{better_num.to_s} is a bigger and better number."
+
+=begin
+### Reflection Section
+
+
+=end
