29827fa889de35c2a4c11760d64003b77903157e
Kevin Huang <kevin.ziwen.huang@gmail.com>
2015-12-10 23:54:24 -0800

    Add 5.2 files

---
+++ week-5/pad-array/my_solution.rb
@@ -0,0 +1,83 @@
+# Pad an Array
+
+# I worked on this challenge with Jordan Fox
+
+# I spent [] hours on this challenge.
+
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+
+
+# 0. Pseudocode
+
+# What is the input? given array, array size, possible value to pad
+# What is the output? (i.e. What should the code return?)
+#the given array in the min size
+# What are the steps needed to solve the problem?
+# Compare size of given array with min_size
+# if array >= min_size, return array
+# else pad array with nil or given value
+
+# 1. Initial Solution
+##def pad!(array, min_size, value = nil) #destructive
+##  if array.length >= min_size
+##    return array
+##  else
+##    count = min_size - array.length
+##    count.times do 
+##      array.push(value)
+##    end
+##  end
+##  return array
+##end
+##
+
+##def pad(array, min_size, value = nil) #non-destructive
+##  temp_array = []
+##  array.each {|x| temp_array.push(x)}
+##  if temp_array.length >= min_size
+##    return temp_array
+##  else
+##    count = min_size - temp_array.length
+##    count.times do 
+##      temp_array.push(value)
+##    end
+##  end
+##  return temp_array
+##end
+
+
+# 3. Refactored Solution
+def pad!(array, min_size, value = nil) #destructive
+  if min_size == 0 || array.size >= min_size
+    array
+  else
+  array.concat(Array.new(min_size - array.size, value))
+  end
+end
+
+def pad(array, min_size, value = nil) #non-destructive
+  temp_array = array.clone
+  pad!(temp_array, min_size, value)
+end
+
+
+# 4. Reflection
+####Were you successful in breaking the problem down into small steps?
+#
+# => Yes! We were able to break down the problem into small steps to make writing the program easier.
+#
+####Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
+#
+# => Yes, since we broke down the basic steps of the program, it was fairly simple to translate it into code.
+# => The main difficulties were that we had were because we did not realize that assigning an array value of another array (array1 = array2), links the two arrays together.
+# => Normally with other programming languages this doesn't happen, so it was a bit surprising to us when we encountered this error.
+# => We were able to successfully circumvent this challenge by figuring out a solution to copying array contents to another array.
+#
+####Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
+####When you refactored, did you find any existing methods in Ruby to clean up your code?
+####How readable is your solution? Did you and your pair choose descriptive variable names?
+####What is the difference between destructive and non-destructive methods in your own words?
