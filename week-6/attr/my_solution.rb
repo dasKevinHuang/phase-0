c8294cbf1bb8bb2fdacbfcf5a742ffb8c4300c10
Kevin Huang <kevinziwenhuang@gmail.com>
2015-12-21 06:19:51 -0800

    Update my_solution.rb

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -1,8 +1,9 @@
 #Attr Methods
+#Written by Kevin Huang
 
-# I worked on this challenge [by myself, with:]
+# I worked on this challenge by myself
 
-# I spent [#] hours on this challenge.
+# I spent 1 hours on this challenge.
 
 # Pseudocode
 
