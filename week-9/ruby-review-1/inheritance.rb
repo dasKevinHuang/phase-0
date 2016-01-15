5817a1489cc039741d9f57b6c226f8aace528aee
Kevin Huang <kevinziwenhuang@gmail.com>
2016-01-14 23:57:38 -0800

    Add reflection section

--- week-9/ruby-review-1/inheritance.rb
+++ week-9/ruby-review-1/inheritance.rb
@@ -222,3 +222,7 @@ p local.email_list
 
 
 # Reflection
+
+#What concepts did you review in this challenge?
+#What is still confusing to you about Ruby?
+#What are you going to study to get more prepared for Phase 1?
