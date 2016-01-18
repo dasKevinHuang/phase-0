e8dd1945f2326f07f10d3d1340bdc5ffc39215f5
Kevin Huang <kevin.ziwen.huang@gmail.com>
2016-01-18 02:28:56 -0800

    add jquery challenge

--- week-9/jquery/jquery_example.js
+++ week-9/jquery/jquery_example.js
@@ -1,8 +1,8 @@
 // U3.W9:JQuery
 
 
-// I worked on this challenge [by myself, with: ].
-// This challenge took me [#] hours.
+// I worked on this challenge with Alana Hanson
+// This challenge took me 1 hours.
 
 $(document).ready(function(){
 
@@ -14,7 +14,7 @@ $(document).ready(function(){
   //Link this script and the jQuery library to the jQuery_example.html file and analyze what this code does.
 
 $('body').css({'background-color': 'pink'})
-
+$('img').css({'border' : '1px black dashed'})
 //RELEASE 2:
   //Add code here to select elements of the DOM
 headline = $("h1")
@@ -26,31 +26,32 @@ $('body > h1').css({'color': 'green', 'border' : '1px solid black', 'visibility'
 $('.mascot > h1').html('Squirrels')
 //RELEASE 4: Event Listener
   // Add the code for the event listener here
+$('img').on('mouseenter', function(e) {
+ e.preventDefault();
+ $(this).attr('src', 'http://assets.inhabitots.com/wp-content/uploads/2013/01/Squirrel-Slam.jpg');
+ })
 
+$('img').on('mouseleave', function(e) {
+ e.preventDefault();
+ $(this).attr('src', 'dbc_logo.png');
+ })
 
 //RELEASE 5: Experiment on your own
-$('img').on('mouseenter', function(e) {
-	e.preventDefault()
-	$(this).attr('src', 'http://assets.inhabitots.com/wp-content/uploads/2013/01/Squirrel-Slam.jpg')
-	})
-$('img').on('mouseleave', function(e) {
-	e.preventDefault()
-	$(this).attr('src', 'dbc_logo.png')
-	})
 
 
-$('img').on('mouseenter', function() {$(this).animate({
-    width: "show",
-    height: "show",
-  }, {
-    duration: 1000000,
-    specialEasing: {
-      width: "linear",
-      height: "easeOutBounce"
-    },
-        complete: function() {
-      $( this ).after( "<div>Animation complete.</div>" );}})
+$('.mascot').on('mousedown', function() {
+  $('img').animate({'opacity': '0.5', 'width' : '90%', 'border-width' : '10px'})
 })
 
-
 }) // end of the document.ready function: do not remove or write DOM manipulation below this.
+
+// Reflection
+
+// What is jQuery?
+// jQuery is a javascript library.
+
+// What does jQuery do for you?
+// It makes it very easy to work within the DOM.
+
+// What did you learn about the DOM while working on this challenge?
+// This challenge helped me practice how to access and change elements within the DOM.
